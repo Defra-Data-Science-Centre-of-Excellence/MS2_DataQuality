4cdc4a0e58aed5d689e99e9af45b3e7d6dee8cf1
dom-mb-defra <dom.benton@defra.gov.uk>
2021-03-09 13:15:23 +0000

    Added .idea to gitignore

---
+++ scriptContainer/main.py
@@ -0,0 +1,20 @@
+# Display command line help
+
+# Parse Command Line Arguments
+
+# Load config file
+
+# Connect to S3
+
+# Traverse S3 directories
+
+# Ingest manifest
+
+# Determine type of file
+
+# Run analysis on file type - compute data quality
+
+# Join with manifest data
+
+# Export CSV locally + upload to S3?
+
