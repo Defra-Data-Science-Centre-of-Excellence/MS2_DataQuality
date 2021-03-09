bf25ff97c0a3ef3861b44804030ff7e0f44e9243
dom-mb-defra <dom.benton@defra.gov.uk>
2021-03-09 21:14:32 +0000

    Added docstrings Relabelled scripContainer package as app Added csv handling unit test Added Config File template Added auxiliary functions Added Command Line interface for main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-# Display command line help
+import main_aux
+import json
 
 # Parse Command Line Arguments
+args = main_aux.parse_args()
+mode = args.mode
+configPath = args.config
 
 # Load config file
+config = main_aux.load_json
 
 # Connect to S3
 
@@ -18,3 +23,6 @@
 
 # Export CSV locally + upload to S3?
 
+
+# if __name__ == '__main__':
+#     pass
---
+++ app/main_aux.py
@@ -0,0 +1,54 @@
+import argparse
+import json
+import sys
+import logging
+
+
+def create_logger():
+    pass
+
+
+def parse_args():
+    """
+    Argument parser function to ensure users input the mode of the script and the path to the config file.
+    :return object args: ArgParser Namespace object that contains key value pairs accessible by attribute names where
+    values are the values provided by the user as a string
+    """
+    arg_parser = argparse.ArgumentParser(description="A command line tool to run metadata creation and "
+                                                     "data quality analysis on ELMS data held in cloud storage.",
+                                         usage='%(prog)s [options]')
+    arg_parser.add_argument('mode',
+                            type=str,
+                            help='choose between "dq" for data quality, or "metadata" for metadata generation')
+    arg_parser.add_argument('config',
+                            type=str,
+                            help='full path to the config file')
+    args = arg_parser.parse_args()
+    _validate_args(args)
+    return args
+
+
+def _validate_args(args):
+    """
+    Light function to ensure the mode given by the user conforms to either of 'dq' or 'metadata' to ensure conformity
+    for the rest of the script. Prints error and exits script if conformity not met.
+    :param object args: ArgParser Namespace object
+    """
+    if args.mode not in ['dq', 'metadata']:
+        print("Improper mode given as argument, please choose from:\n- dq\n- metadata\n\nExiting.")
+        sys.exit()
+
+
+def load_json_file(file_path):
+    """
+    Function to upload JSON files into dictionaries given the file path.
+    :param string file_path: File path string to json file
+    :return dict file: Dict representation of the file
+    """
+    with open(file_path, 'r') as f:
+        file = json.load(f)
+    return file
+
+
+def validate_config_file(config):
+    pass
