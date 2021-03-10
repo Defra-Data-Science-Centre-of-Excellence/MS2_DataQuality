063d5c90b61ca253bc9ee1bd19c0b7cf82aa2ab0
benjaminellis-ecg <benjamin.ellis@defra.gov.uk>
2021-03-10 11:10:52 +0000

    adding progress on crawler and cdsm

--- app/Crawler/CloudDataStorageManager/cloud_data_storage_manager.py
+++ app/Crawler/CloudDataStorageManager/cloud_data_storage_manager.py
@@ -4,10 +4,12 @@ TODO
     - Implement upload functionality
     - Test get manifest file functionaltiy
     - implement _load_manifest_file for json
+    - what happens if you try to acesss a bucket that doesn't exist, this should be auidted
+    - docstrings
+    - verify caching is correct
 """
 
 import boto3
-from botocore.client import BaseClient
 import json
 import re
 from functools import lru_cache
@@ -16,13 +18,13 @@ from typing import Union
 
 class CloudDataStorageManager(object):
     """
-    Class to manage cloud data storage.
+    CloudDataStorageManager is a class used to handle interactions with AWS S3 buckets
 
     Usage:
 
-    cdsm = CloudDataStorageManager(credentials_fp = "some/fp/here",
+    cdsm = CloudDataStorageManager(credentials_fp = "some/fp/here")
 
-    all_dataset_file = cdsm.get_dataset_files(bucket = "elms-test-1")
+    all_dataset_file = cdsm.get_dataset_files_list(bucket = "elms-test-1")
 
     """
 
@@ -46,7 +48,7 @@ class CloudDataStorageManager(object):
                                     )
         self._list_object_paginator = self._client.get_paginator('list_objects')
 
-    def get_dataset_files(self, bucket: str) -> list:
+    def get_dataset_files_list(self, bucket: str) -> list:
 
         """
         Get all dataset files in the specified bucket
@@ -74,65 +76,72 @@ class CloudDataStorageManager(object):
 
         return dataset_files_to_return
 
-    def get_manifest_file(self, bucket: str,
-                          manifest_directory: str) -> Union[None, json]:
+    def get_manifest_file(self,
+                          bucket: str,
+                          manifest_directory: str) -> Union[None, dict]:
         """
-
+        Method to return the manifest file requested as a FLO
         :param bucket:
         :param manifest_directory:
         :return:
         """
         page_iterator = self._list_object_paginator.paginate(Bucket = bucket,
-                                                             prefix = manifest_directory)
+                                                             Prefix = manifest_directory)
         for page in page_iterator:
 
             if len(page['Contents']) == 2:
-
                 count = 0
 
                 for entry in page['Contents']:
-
                     count += 1
 
-                    if entry["Key"] == "manifest.json":
-
-                        manifest_file = _load_manifest_file(client = self._client,
-                                                            bucket = bucket,
-                                                            key = entry["Key"])
+                    if "manifest.json" in entry["Key"]:
+                        manifest_file = self._load_manifest_file(bucket = bucket,
+                                                                 key = entry["Key"])
                         return manifest_file
+
                     else:
                         # only audit this if both paths have been checked and the manifest file doesn't have the
                         # expected name and extension
                         if count == 2:
                             print(
-                                "ERROR: could not find manafest file with expected extension .json in the location: {}"
-                                .format(manifest_directory))
+                                f"ERROR: could not find manifest file manifest.json in directory {manifest_directory}")
 
             # auditing errors for not finding manifest file
             elif len(page['Contents']) > 2:
-                print("ERROR: found more than one manifest file in folder {}".format(page['Contents'][0]["Key"]))
+                print(f"ERROR: found more than one manifest file in directory {manifest_directory}")
+
             else:
-                print("ERROR: no manifest file found in folder {}".format(page['Contents'][0]["Key"]))
+                print(f"ERROR: no manifest file found in directory {manifest_directory}")
 
+    def read_file_from_storage(self, bucket: str, key: str) -> str:
+        """
 
-@lru_cache(maxsize = 2)
-def _load_manifest_file(client: BaseClient,
-                        bucket: str,
-                        key: str):
-    """
-    Gets manifest file from bucket, using cache to reduce calls to the server
-    :param client:
-    :param bucket:
-    :param key:
-    :return:
-    """
-    obj = client.get_object(Bucket = bucket, Key = key)
-    # used to read dummy file, this needs to be json
-    manifest_file = pd.read_csv(obj['Body'])
-    return manifest_file
+        :param bucket:
+        :param key:
+        :return: FLO object
+        """
+        obj = self._client.get_object(Bucket = bucket, Key = key)
+        return obj['Body']
+
+    @lru_cache(maxsize = 2)
+    def _load_manifest_file(self, bucket: str, key: str) -> dict:
+        """
+
+        :param bucket:
+        :param key:
+        :return: dict
+        """
+        manifest_flo = self.read_file_from_storage(bucket = bucket, key = key)
+        return json.load(manifest_flo)
+
+    def __str__(self):
+        return "CloudDataStorageManager Object"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 if __name__ == "__main__":
     cdsm = CloudDataStorageManager(credentials_fp = "C:/Users/beellis/aws_creds.json")
-    dataset_file = cdsm.get_dataset_files(bucket = "elms-test-1")
-    print(dataset_file)
+    dataset_file = cdsm.get_dataset_files_list(bucket = "elms-test-1")
--- app/Crawler/crawler.py
+++ app/Crawler/crawler.py
@@ -2,13 +2,19 @@
 TODO:
     - could this be done in parallel?
     - I need to move the dataHandlers into the crawler packae? I can't access them as is
+    - add in audit statments so progress can be tracked for logs
+    -
 """
 
-from .CloudDataStorageManager import CloudDataStorageManager
-from os.path import dirname
+from app.Crawler.CloudDataStorageManager import CloudDataStorageManager
+from app.dataHandlers import *
+from os.path import dirname, splitext
 
 
 class Crawler(object):
+    """
+
+    """
 
     def __init__(self, credentials_fp: str):
         """
@@ -22,13 +28,18 @@ class Crawler(object):
         :param bucket:
         :return:
         """
-        dataset_files = self._cdsm.get_dataset_files(bucket = bucket)
+        dataset_files = self._cdsm.get_dataset_files_list(bucket = bucket)
 
         for dataset_file in dataset_files:
             dataset_dir_name = dirname(dataset_file["Key"]).split("/")[0]
-            manifest_directory = "{}/{}/".format(dataset_dir_name, "manifest")
+            manifest_directory = f"{dataset_dir_name}/manifest/"
             manifest_file = self._cdsm.get_manifest_file(bucket = bucket, manifest_directory = manifest_directory)
-            # know we need to create the metadata
+            if manifest_file is None:
+                print(f"ERROR: no manifest file returned, creation of metadata file for dataset file {dataset_file['Key']} aborted")
+                   # this needs to be tested
+            else:
+                created_metdata_file = self._create_dataset_file_metadata(bucket = bucket, dataset_file = dataset_file)
+
 
     def create_metadata_for_buckets(self, buckets: list) -> None:
         """
@@ -47,3 +58,46 @@ class Crawler(object):
         # TODO build sprint 4
         for bucket in bucket:
             self.create_data_quality_for_bucket(bucket = bucket)
+
+    def _create_dataset_file_metadata(self, bucket: str, dataset_file: dict) -> list:
+        """
+        Loads file from storage and returns the metadata
+        TODO - how do we return with and without layers
+        :return:
+        """
+        shape_file_formats = [".shp", ".shx", ".shb"]
+        _, dataset_file_extension = splitext(dataset_file["Key"])
+        dataset_file_flo = self._cdsm.read_file_from_storage(bucket = bucket, key = dataset_file["Key"])
+        if dataset_file_extension in shape_file_formats:
+            # disspacth to shape file data handler
+            print(f"ERROR: dataset file is Shape file format, this is currently not supported")
+            return [None]
+        elif dataset_file_extension == ".json":
+            try:
+                header_list, num_rows = create_geojson_metadata(file = dataset_file_flo)
+            except:
+                # this except is too broad
+                print(f"ERROR: tried to load file {dataset_file} as GEOjson but failed. Only GEOjson formats of json files are currently supported")
+                return [None]
+        elif dataset_file_extension == ".csv":
+            header_list, num_rows = create_csv_metadata(file = dataset_file_flo)
+            return [None]
+        elif dataset_file_extension ==  ".gpkg":
+            layers, headers_list, num_rows = create_gpkg_metadata(file = dataset_file_flo)
+            return [None]
+        else:
+            print(f"ERROR: did not recognise file extension {dataset_file_extension} for file {dataset_file['Key']}")
+            return [None]
+
+
+    def __str__(self):
+        return "Crawler Object"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+if __name__ == "__main__":
+    bucket = "elms-test-1"
+    c = Crawler(credentials_fp = "C:/Users/beellis/aws_creds.json")
+    c.create_metadata_for_bucket(bucket = bucket)
