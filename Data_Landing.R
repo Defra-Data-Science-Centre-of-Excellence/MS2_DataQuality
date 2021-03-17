83e142ab666b50001f1c425275babc0bf3951973
HSethiIDE <hasdeep.sethi@capgemini.com>
2021-03-17 13:56:46 +0000

    Setting up Data_Landing.R

---
+++ Data_Landing.R
@@ -0,0 +1,23 @@
+
+## Load packages required for Data Quality (DQ) Tool ##
+library('readxl') ## Used to ingest Spreadsheets ##
+library('dplyr') ## Data Manipulation ##
+library(aws.s3) ## Needed to interact with AWS S3 ##
+library(aws.ec2metadata) ## Needed to interact with AWS S3 ##
+
+Sys.setenv("AWS_DEFAULT_REGION" = 'eu-west-1')
+
+### GLOBAL SETTINGS ###
+## How many metrics are in the DQ tool (excluding dataset name and column name)? ##
+Metrics = 6 
+
+### Loading in local files (placeholder) ##
+Data <- read_excel("Data_quality_1_snapshot_17.3.2021.xlsx")
+
+## Filter columns based on global setting Metrics ##
+## This removes any stray columns and reduces ambiguity ##
+Data <- Data[,c(1:(Metrics+2))] ## Number of metrics + 2 cols for dataset name and column name 
+
+
+## Loading in all outputs from AWS S3 ## 
+## TBU ## 
