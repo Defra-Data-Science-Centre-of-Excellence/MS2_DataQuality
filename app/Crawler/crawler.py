0a69beced19cf3b54e8d88299f54422277a205bb
dom-mb-defra <dom.benton@defra.gov.uk>
2021-03-17 15:57:27 +0000

    Added logging steps, reduced logging levels of non-urgent errors such as file misreads

--- app/Crawler/crawler.py
+++ app/Crawler/crawler.py
@@ -49,7 +49,8 @@ class Crawler(object):
 
         if dataset_files is None:
             # no bucket exists so we get no returned files
-            self.logger.error(f"ERROR: aborting metadata creation for bucket {bucket}")
+            self.logger.error(f"ERROR: aborting metadata creation for bucket {bucket}. "
+                              f"Bucket is empty or does not exist.")
         else:
             csv_data = []
             sfc = None
@@ -63,8 +64,8 @@ class Crawler(object):
                 manifest_file = self._cdsm.get_manifest_file(bucket=bucket, manifest_directory=manifest_directory)
 
                 if manifest_file is None:
-                    self.logger.error(f"ERROR: no manifest file returned, creation of metadata file for dataset "
-                                      f"file {dataset_file['Key']}. Aborted operation.")
+                    self.logger.debug(f"WARNING: no manifest file returned, creation of metadata file for dataset "
+                                      f"file {dataset_file['Key']} aborted.")
 
                 else:
                     dataset_file_extension = self._get_file_extension(dataset_file)
@@ -100,7 +101,7 @@ class Crawler(object):
                     # TODO should this be a if, else??? This way we don't create metadata output files if we don't get
                     # created metadata back. I think if we remove the else here it should work better
                     if created_dataset_metadata is None:
-                        self.logger.warning(f"WARNING: unable to create metadata for dataset file "
+                        self.logger.debug(f"WARNING: unable to create metadata for dataset file "
                                             f"{dataset_file['Key']}")
 
                     else:
@@ -135,6 +136,8 @@ class Crawler(object):
                             else:
                                 metadata_row.append("")
                         csv_data.append(metadata_row)
+                        self.logger.info(f"Successfully processed dataset {dataset_dir_name[0]}........"
+                                         f"{dataset_dir_name[-1]}.{dataset_file['Key'].split('.')[-1]}")
             return csv_data
 
     def create_metadata_for_buckets(self, buckets: list) -> list:
@@ -194,7 +197,7 @@ class Crawler(object):
         dataset_file_flo = self._cdsm.read_file_from_storage(bucket = bucket, key = dataset_file["Key"])
 
         if dataset_file_extension in self._companion_json["shape_file_extensions"]:
-            self.logger.error(f"ERROR: dataset file is Shape file format, this is currently not supported")
+            self.logger.debug(f"ERROR: dataset file is Shape file format, this is currently not supported")
             return None
 
         elif dataset_file_extension == ".json":
@@ -204,7 +207,7 @@ class Crawler(object):
 
             except Exception as e:
                 self.logger.exception(e)
-                self.logger.error(f"ERROR: tried to load file {dataset_file} as GEOjson but failed. Only GEOjson "
+                self.logger.debug(f"ERROR: tried to load file {dataset_file} as GEOjson but failed. Only GEOjson "
                                   f"formats of json files are currently supported")
                 return None
 
@@ -227,7 +230,7 @@ class Crawler(object):
                 return None
 
         else:
-            self.logger.error(f"ERROR: did not recognise file extension {dataset_file_extension} for "
+            self.logger.debug(f"ERROR: did not recognise file extension {dataset_file_extension} for "
                               f"file {dataset_file['Key']}")
             return None
 
