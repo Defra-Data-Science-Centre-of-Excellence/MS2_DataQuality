a72bcbfabf49e9a87df7625be0e8e0101868d8f7
benjaminellis-ecg <benjamin.ellis@defra.gov.uk>
2021-03-23 13:18:09 +0000

    hot fix

--- app/data_quality/data_quality_function.py
+++ app/data_quality/data_quality_function.py
@@ -122,7 +122,8 @@ def create_dq_reports(logger, gdf_list: list, file_dict: dict) -> list:
                 # If Pct = 1, then the object should be a float/integer
                 # Note, we are using len(Temp[checker]) and not Length, as the removal of NAs may generate a
                 # difference between the two
-                pct = temp['checker'].sum() / len(temp['checker'])
+                # pct = temp['checker'].sum() / len(temp['checker'])
+                pct = np.sum(temp['checker'].to_numpy()) / len(temp['checker'].tolist())
 
                 object_type_checker.append([col, pct])
 
